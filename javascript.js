8a1b36be5a53c0080d438284ae0d0b6046ebffd5
AntonioMPereira <antoniofmpereira@gmail.com>
2022-12-19 17:15:51 +0000

    Finished pvp mode. p1 victory, p2 victory and draw all work.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,109 @@
-const gameDisplay = document.querySelector('.game-display');
+const gameWindows = document.querySelector('.game-windows');
 const game = (() => {
 
   const start = () => {
     const startMenu = document.querySelector('.start-menu-display');
     document.querySelectorAll('.start-btn').forEach(btn => btn.addEventListener('click', () => {
+      document.getElementById('btn-reset').style.display = "block";
+      document.getElementById('btn-reset').textContent = "Quit";
       startMenu.style.display = "none";
-      if (btn.id === "btn-pvp") game.playPvp();
-      if (btn.id === "btn-pve") game.playPve();
-      if (btn.id === "btn-rules") game.showRules();
+      if (btn.id === "btn-pvp") {
+        let gameMode = "pvp";
+        game.randomize(gameMode);
+      } else if (btn.id === "btn-pve") {
+        let gameMode = "pve";
+        game.randomize(gameMode);
+      } else if (btn.id === "btn-rules") {
+        game.showRules();
+      };
     }));
   };
 
   const playPvp = () => {
-      game.randomize();
+    let playCounter = 0;
+    document.querySelectorAll('.table-box').forEach(box => box.addEventListener('click', (e) => {
+      if (playCounter % 2 === 0) {
+        e.target.innerText = playerOne.symbol;
+        playerOne.tracker.push(e.target.id);
+        checkWinner(playerOne);
+      } else if (playCounter % 2 !== 0) {
+        e.target.innerText = playerTwo.symbol;
+        playerTwo.tracker.push(e.target.id);
+        checkWinner(playerTwo);
+      };
+      function checkWinner(player) { 
+        winConditions = [
+        ["b0", "b1", "b2"],
+        ["b0", "b3", "b6"],
+        ["b0", "b4", "b8"],
+        ["b1", "b4", "b7"],
+        ["b3", "b4", "b5"],
+        ["b2", "b4", "b6"],
+        ["b2", "b5", "b8"],
+        ["b6", "b7", "b8"],
+        ];
+        for (const element of winConditions) {
+          let points = 0;
+          for (value of element) {
+            for (let i = 0; i < player.tracker.length; i++) {
+              if (value === player.tracker[i]) points += 1;
+              if (points === 3) return winRound(player);  
+            }
+          }
+        }
+        e.target.inert = true;
+      };
+      playCounter++;
+      if (playCounter === 10) console.log("draw");
+      
+      //missing function for draws!
+
+      // sometimes between rounds the same player plays twice in a row! 
+
+
+    }));  
+    function winRound(player) {
+      player.score += 1;
+      document.querySelector('.player-one-display').textContent = `${playerOne.name}: ${playerOne.score}`;
+      document.querySelector('.player-two-display').textContent = `${playerTwo.name}: ${playerTwo.score}`;
+      playCounter = 0;
+      playerOne.tracker = [];
+      playerTwo.tracker = [];
+      document.querySelectorAll('.table-box').forEach(box => {
+        box.innerText = "";
+        box.inert = false;
+      });      
+
+      
+    };
   };
 
   const playPve = () => {
-      game.randomize();
   };
 
   const showRules = () => {
+    document.getElementById('btn-reset').textContent = "Go Back";
 
+    const rulesContainer = document.createElement('div');
+    rulesContainer.classList.add('rules-container','container');
+    gameWindows.appendChild(rulesContainer);
+    const rulesTitle = document.createElement('div');
+    rulesTitle.classList.add('row', 'rules-row','rules-row-title');
+    rulesTitle.innerHTML = "Rules";
+    rulesContainer.appendChild(rulesTitle);
+    
+    const rulesRowOne = document.createElement('div');
+    rulesRowOne.classList.add('row','rules-row');
+    rulesContainer.appendChild(rulesRowOne);
+    const rulesRowOneParagraph = document.createElement('p');
+    rulesRowOneParagraph.innerHTML = "Two players take turns marking the spaces in a three-by-three grid with <strong>X</strong> or <strong>O</strong>. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.";
+    rulesRowOne.appendChild(rulesRowOneParagraph);
   };
 
-  const randomize = () => {
+  const randomize = (gameMode) => {
     const randomizeContainer = document.createElement('div');
     randomizeContainer.classList.add('row','randomize-container');
-    gameDisplay.appendChild(randomizeContainer);
+    gameWindows.appendChild(randomizeContainer);
     const randomizer = document.createElement('div');
     randomizer.classList.add('randomizer','col-12','row');
     randomizeContainer.appendChild(randomizer); 
@@ -37,8 +114,8 @@ const game = (() => {
       if (num % 2 !== 0) randomizer.textContent = "O";
       if (num % 2 === 0) randomizer.textContent = "X";
       sortedNum += Math.floor(Math.random() * 2  + 1);
-      //edit progress number after completion
-      if (sortedNum >= 5) {
+      //edit sortedNum after completion
+      if (sortedNum >= 1/*35*/) {
         clearInterval(getXO);
         randomizer.style.transition = "500ms";
         randomizer.style.fontSize = "2rem";
@@ -52,7 +129,8 @@ const game = (() => {
         startBtnContainer.classList.add('start-btn-container','row');
         randomizeContainer.appendChild(startBtnContainer);
         const startBtn = document.createElement('button');
-        startBtn.classList.add('start-btn');
+        startBtn.classList.add('game-btn');
+        startBtn.setAttribute('id','btn-start');
         startBtnContainer.appendChild(startBtn);
         startBtn.innerText = "Start Game";
         playerOne.symbol = randomizer.textContent;
@@ -61,50 +139,51 @@ const game = (() => {
         startBtn.addEventListener('click', () => {
           randomizeContainer.style.display = "none";
           gameBoard.generate();
-          game.playRoundPvp();
+          document.querySelector('.player-one-display').textContent = `${playerOne.name}: ${playerOne.score}`;
+          document.querySelector('.player-two-display').textContent = `${playerTwo.name}: ${playerTwo.score}`;
+          if (gameMode === "pvp") game.playPvp();
+          if (gameMode === "pve") game.playPve();
         });
       };
     }, 100);
   };
 
-  const playRoundPvp = () => {
-    document.querySelectorAll('.table-box').forEach(box => box.addEventListener('click', (e) => {
-      e.target.classList.add('cross');
-      e.target.inert = true;
-      e.target.innerText += "X";
-    }));
-  };
-
   const reset = () => {
     document.getElementById('btn-reset').addEventListener('click', () => {
-      if (document.querySelector('.randomize-container') !== null) {
-        document.querySelector('.randomize-container').remove();
+      while (gameWindows.firstChild) {
+        gameWindows.removeChild(gameWindows.firstChild);
       };
-      if (document.querySelector('.game-table') !== null) {
-        document.querySelector('.game-table').remove();
-      }
       document.querySelector('.start-menu-display').style.display = "flex";
+      document.getElementById('btn-reset').style.display = "none";
+      playerOne.score = 0;
+      document.querySelector('.player-one-display').textContent = "";
+      playerTwo.score = 0;
+      document.querySelector('.player-two-display').textContent = "";
+      playerOne.tracker = [];
+      playerTwo.tracker = [];
+      document.querySelectorAll('.table-box').forEach(box => box.inert = false);
+
     });
+
   };
 
-  return {start, playPvp, playPve, showRules, randomize, playRoundPvp, reset};
+  return {start, playPvp, playPve, showRules, randomize, reset};
 })();
 
-
 const gameBoard = (() => {
-  const board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  const board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
   const generate = () => {
     const gameTable = document.createElement('div');
     gameTable.classList.add('container','game-table');
-    gameDisplay.appendChild(gameTable);
+    gameWindows.appendChild(gameTable);
     for (let i = 0; i < board.length; i++) {
       const tableRow = document.createElement('div');
-      tableRow.setAttribute('id', `table-row-${i + 1}`);
+      tableRow.setAttribute('id', `table-row-${i}`);
       tableRow.classList.add('row', 'table-row');
       gameTable.appendChild(tableRow);
       for (let j = 0; j < board[i].length; j++) {
         const tableBox = document.createElement('div');
-        tableBox.setAttribute('id', `box-${board[i][j]}`);
+        tableBox.setAttribute('id', `b${board[i][j]}`);
         tableBox.classList.add('col','table-box');
         tableRow.appendChild(tableBox);
       }
@@ -114,12 +193,13 @@ const gameBoard = (() => {
 
 })();
 
-const playerFactory = (name, symbol) => {
-  return {name, symbol};
+const playerFactory = (name, symbol, tracker, score) => {
+  return {name, symbol, tracker, score};
 }
 
-const playerOne = playerFactory('Player 1', null);
-const playerTwo = playerFactory('player 2', null);
+const playerOne = playerFactory('Player One', null, [], 0);
+const playerTwo = playerFactory('Player Two', null, [], 0);
+const cpu = playerFactory('CPU', null, [], 0);
 
 game.start();
 game.reset();
